69c151cf6a64f185ff8b6d1d57bf763a8da06dde
Nick <nickpirr@aol.com>
2017-11-27 16:49:00 -0500

    Configured for PyInstaller exe conversion
    Fixed filepath after --onefile command

--- parser_gui.py
+++ parser_gui.py
@@ -4,17 +4,21 @@
 #User Interface for summary_csv_parser and billing_csv_parser
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QPushButton, QTableWidget,QTableWidgetItem,QGridLayout,QHeaderView, QFileDialog, QInputDialog, QLineEdit
 from PyQt5.QtGui import QIcon
-
+import os
 import sys
 import csv_parser
+import summary_pdf
 
 class Summary(QWidget):
+	
+	def resource_path(self, relative_path):
+		base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+		return os.path.join(base_path, relative_path)
 
 	def __init__(self):
 		super().__init__()
 
 		self.initUI()
-
 
 	def initUI(self):
 
@@ -44,22 +48,31 @@ class Summary(QWidget):
 		grid_layout.addWidget(self.tableWidget,1,1,1,3)
 
 		self.setLayout(grid_layout)
+		path = self.resource_path('axe_logo.png')
+		self.setWindowIcon(QIcon(path))
 
 		#Window init
 		self.setGeometry(300,300,1000,600)
 		self.setWindowTitle('Axe Monthly Summary')
 		self.show()
 
+	def convert_data(self,data):
+		new_data = [['Company','PreTax','PostTax','percentage']]
+
+		for i in range(0,len(data)):
+			new_data.append(list(data[i].values()))
+		return new_data
+
 	def imprt_click(self):
 		filename = QFileDialog.getOpenFileName(self,'Open File','C://','CSV (*.csv)')[0]
 		if filename:
-			data_list = csv_parser.init_parse(filename)
-			self.tableWidget.setRowCount(len(data_list))
-			for row in range(0,len(data_list)):
-				self.tableWidget.setItem(row, 0, QTableWidgetItem(data_list[row]['name']))
-				self.tableWidget.setItem(row, 1, QTableWidgetItem(str(data_list[row]['preproc'])))
-				self.tableWidget.setItem(row, 2, QTableWidgetItem(str(data_list[row]['postproc'])))
-				self.tableWidget.setItem(row, 3, QTableWidgetItem(str(data_list[row]['percentage'])))
+			self.data_list = csv_parser.init_parse(filename)
+			self.tableWidget.setRowCount(len(self.data_list))
+			for row in range(0,len(self.data_list)):
+				self.tableWidget.setItem(row, 0, QTableWidgetItem(self.data_list[row]['name']))
+				self.tableWidget.setItem(row, 1, QTableWidgetItem(str(self.data_list[row]['preproc'])))
+				self.tableWidget.setItem(row, 2, QTableWidgetItem(str(self.data_list[row]['postproc'])))
+				self.tableWidget.setItem(row, 3, QTableWidgetItem(str(self.data_list[row]['percentage'])))
 
 
 	def reset_click(self):
@@ -70,8 +83,12 @@ class Summary(QWidget):
 	def savePDF_click(self):
 		text, okPressed = QInputDialog.getText(self,"PDF Saving","PDF File Name:",QLineEdit.Normal,"")
 		if okPressed and text != '':
-			print(text)
 
+			parsed_data = self.convert_data(self.data_list)
+			print(parsed_data)
+			print(text)
+			doc = summary_pdf.PdfSummary(text+".pdf",parsed_data)
+			doc.gen_pdf()
 			
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
--- summary_pdf.py
+++ summary_pdf.py
@@ -2,50 +2,83 @@
 #Nicholas Pirrello
 #11/08/17
 #PDF generator for Summary reports
-from reportlab.platypus import Paragraph, Table, Image
+from reportlab.platypus import Paragraph, Table, Image, TableStyle, SimpleDocTemplate
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 from reportlab.lib.units import inch, mm
-
+import os.path
+import os
+import sys
+import time
+import datetime
 #Reference of https://www.blog.pythonlibrary.org/2012/06/27/reportlab-mixing-fixed-content-and-flowables/
 class PdfSummary():
 
-	def __init__(self, filename):
-		self.c = canvas.Canvas(filename,pagesize=letter)
+	def resource_path(self, relative_path):
+		base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+		return os.path.join(base_path, relative_path)
+
+	def __init__(self, filename, data):
+		i = 1
+		original_name = filename
+		while os.path.isfile(filename):
+			docname = filename[0:len(original_name) - 4]
+			extension = '.pdf'
+			filename = docname + '(' + str(i) + ')' + extension
+			i+=1
+		self.file = filename
+		self.doc = SimpleDocTemplate(filename,pagesize=letter,rightMargin=30,leftMargin=30,topMargin=30,bottomMargin=18)
+		self.elements = []
 		self.styles = getSampleStyleSheet()
-		self.width, self.height = letter
+		self.stylesWrap = self.styles["BodyText"]
+		self.data = data
+
 
 	def gen_pdf(self):
 		#Create Header bar
-		words = """<font size="16">Summary Billing Report</font>"""
-		header_para = Paragraph(words, self.styles["Heading1"])
 
-		logo = Image("axe_logo.png")
-		logo.drawHeight = 1*inch
-		logo.drawWidth = 1*inch
+		path = self.resource_path('axe_logo_pdf.png')	
+		logo = Image(path)
 
-		headerbr = [[logo,header_para]]
-		header = Table(headerbr, colWidths=4*inch)
-		header.setStyle([("VALIGN",(0,0),(0,0),"TOP")])
-		header.wrapOn(self.c,self.width,self.height)
-		header.drawOn(self.c,*self.coord(18,60,mm))
+		self.elements.append(logo)
+
+		#Description
+		description = """<font size="12">This table has been outputted through the Billing Summary application's pdf generator. This file, {}, was generated {}.</font>""".format(self.file,datetime.datetime.now().strftime("%m-%d-%y"))
+		desc = Paragraph(description, self.stylesWrap)
+
+
+		self.elements.append(desc)
+
+		bufferwords = Paragraph("",self.stylesWrap)
+
+		self.elements.append(bufferwords)
 
 		#Create data table
+		style_table = TableStyle([('ALIGN',(1,1),(-2,-2),'RIGHT'),
+                       ('TEXTCOLOR',(1,1),(-2,-2),colors.red),
+                       ('VALIGN',(0,0),(0,-1),'TOP'),
+                       ('TEXTCOLOR',(0,0),(0,-1),colors.blue),
+                       ('ALIGN',(0,-1),(-1,-1),'CENTER'),
+                       ('VALIGN',(0,-1),(-1,-1),'MIDDLE'),
+                       ('TEXTCOLOR',(0,-1),(-1,-1),colors.green),
+                       ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                       ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                       ])
+
+		wrap = self.stylesWrap
+		wrap.wordWrap = 'CJK'
+
+		data2 = [[Paragraph(cell,wrap) for cell in row] for row in self.data]
+		data_table = Table(data2)
+		data_table.setStyle(style_table)
+
+		self.elements.append(data_table)
+
+		self.doc.build(self.elements)
 
 	def coord(self, x, y, unit=1):
 		x, y = x * unit, self.height - y * unit
 		return x, y
-
-	def savePDF(self):
-		self.c.save()
-
-def main():
-	doc = PdfSummary("test4.pdf")
-	doc.gen_pdf()
-	doc.savePDF()
-
-if __name__ == '__main__':
-	main()
-
